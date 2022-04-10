06ea204af32977a0b19b97632e933c484d1558f6
Андрей <79538233369@ya.ru>
2022-04-10 18:39:48 +0500

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,17 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 7;
+        if (age == 18) {
+            System.out.println("Congrats with graduating school");
+        } else if (age == 21) {
+            System.out.println("Now you are allowed to drink alcohol");
+        } else if (age == 7) {
+            System.out.println("Go to school");
+        } else {
+            System.out.println("there is no news");
+        }
+    }
+}
